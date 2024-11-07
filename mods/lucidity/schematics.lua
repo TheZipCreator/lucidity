613570906ba97774c0729eda5c1ddb8fd679521c
TheZipCreator <thezipcreator@protonmail.com>
2024-11-07 15:43:53 -0500

    trees

---
+++ mods/lucidity/schematics.lua
@@ -0,0 +1,23 @@
+return {
+	tree = {
+		size = {x = 3, y = 4, z = 3},
+		data = {
+			-- layer 1
+			{name = "ignore"}, {name = "ignore"}, {name = "ignore"},
+			{name = "ignore"}, {name = "ignore"}, {name = "ignore"},
+			{name = "lucidity:leaves"}, {name = "lucidity:leaves"}, {name = "lucidity:leaves"},
+			{name = "lucidity:leaves"}, {name = "lucidity:leaves"}, {name = "lucidity:leaves"},
+			-- layer 2
+			{name = "ignore"}, {name = "lucidity:wood"}, {name = "ignore"},
+			{name = "ignore"}, {name = "lucidity:wood"}, {name = "ignore"},
+			{name = "lucidity:leaves"}, {name = "lucidity:wood"}, {name = "lucidity:leaves"},
+			{name = "lucidity:leaves"}, {name = "lucidity:leaves"}, {name = "lucidity:leaves"},
+			-- layer 3
+			{name = "ignore"}, {name = "ignore"}, {name = "ignore"},
+			{name = "ignore"}, {name = "ignore"}, {name = "ignore"},
+			{name = "lucidity:leaves"}, {name = "lucidity:leaves"}, {name = "lucidity:leaves"},
+			{name = "lucidity:leaves"}, {name = "lucidity:leaves"}, {name = "lucidity:leaves"},
+		},
+		flags = "place_center_x, place_center_z"
+	}
+}
